6a5b7378d70715bacfac04093a6a5b4ce64c5a3c
Pablo Eduardo <158394108+devPabloEdu@users.noreply.github.com>
2024-05-05 19:02:05 -0300

    projeto completo

---
+++ script.js
@@ -0,0 +1,26 @@
+const key = "175f59af846a87be0149b2d0596c8c1a";
+
+function colocarDadosNaTela(dados){
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + " ºC"
+
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "% de umidade"
+
+    document.querySelector(".img-previsao").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+}
+
+async function buscarCidade(cidade) {
+   const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json());
+
+   colocarDadosNaTela(dados);   
+}
+
+function cliqueiNoBotao(){
+    let cidade = document.querySelector(".input-cidade").value;
+ 
+    buscarCidade(cidade);
+ }
+
